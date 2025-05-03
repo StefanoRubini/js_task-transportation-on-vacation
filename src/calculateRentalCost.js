9d0554b75fc9abdfde1c2b40c8255934d3b3bdff
Stefano Rubini <stefanorubinijr@gmail.com>
2025-05-02 19:36:41 -0700

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,26 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const DAILY_RENTAL_C0ST = 40;
+  const SHORT_TERM_RENTAL = 3;
+  const LONG_TERM_RENTAL = 7;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+
+  // calculate how much the rental car will cost based on rate and # of days
+  let totalRentalCost = DAILY_RENTAL_C0ST * days;
+
+  // check if the car is rented for 3 or more days, but less than 7 days
+  if (days >= SHORT_TERM_RENTAL && days < LONG_TERM_RENTAL) {
+    totalRentalCost -= SHORT_TERM_DISCOUNT;
+  }
+
+  // check if the car is rented for 7 or more days
+  if (days >= LONG_TERM_RENTAL) {
+    totalRentalCost -= LONG_TERM_DISCOUNT;
+  }
+
+  return totalRentalCost;
 }
 
 module.exports = calculateRentalCost;
